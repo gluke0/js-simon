d5a33fb671d43e25c1ed77a9b291dc0c485c267a
gluke0 <milanesigianluca@gmail.com>
2023-04-04 17:18:26 +0200

    bottone play

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,36 +3,49 @@
 // utente inserisce uno ad uno i numeri che si ricorda
 // software controlla quali numeri sono stati inseriti e controlla quuelli indovinati
 
+function addClass(element, className){
+    element.classList.add(className);
+  }
+
 let rNumbers = [];
 let uNumbers = [];
 let result = [];
 
-let div = document.querySelector(".container");
+let play = document.querySelector('button');
+
+play.addEventListener('click', function(){
+    
+    addClass(play, 'hidden')
+
+    let div = document.querySelector(".container");
  
-for (let i = 0; i < 5; i++){
-    let generated  = Math.floor(Math.random() * 99) + 1;
-    rNumbers.push(generated);
-    div.innerHTML += `<div> ${generated} </div>`;
-}
-
-setTimeout (function(){
-    div.innerHTML = ``;
-}, 4000);
-
-setTimeout (function() {
     for (let i = 0; i < 5; i++){
-        let chosen = parseInt(prompt("Quali numeri ti ricordi?"));
-        uNumbers.push(chosen);
+        let generated  = Math.floor(Math.random() * 99) + 1;
+        rNumbers.push(generated);
+        div.innerHTML += `<div> ${generated} </div>`;
+    }
+    
+    setTimeout (function(){
+        div.innerHTML = ``;
+    }, 4000);
+    
+    setTimeout (function() {
+        for (let i = 0; i < 5; i++){
+            let chosen = parseInt(prompt("Quali numeri ti ricordi?"));
+            uNumbers.push(chosen);
+        };
+    
+        for (let i = 0; i < 5; i++){
+        if (rNumbers[i] == uNumbers[i]){
+            result.push(uNumbers[i]);
+        };
     };
+    
+    div.innerHTML += `<span> hai fatto ${result.length} su 5: ${result}</span>`;
+    div.innerHTML += `<section> I numeri random: ${rNumbers} </section>` 
+    div.innerHTML += `<section> I tuoi numeri: ${uNumbers} </section>`
+    
+    }, 5000);
 
-    for (let i = 0; i < 5; i++){
-    if (rNumbers[i] == uNumbers[i]){
-        result.push(uNumbers[i]);
-    };
-};
+})
 
-div.innerHTML += `<span> hai fatto ${result.length} su 5: ${result}</span>`;
-div.innerHTML += `<section> I numeri random: ${rNumbers} </section>` 
-div.innerHTML += `<section> I tuoi numeri: ${uNumbers} </section>`
-
-}, 5000);
