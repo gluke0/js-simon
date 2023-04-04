77a860411cd36cde63bc1dd9957c85e616cd313b
gluke0 <milanesigianluca@gmail.com>
2023-04-04 16:01:31 +0200

    numeri random +  controllo memoria utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,26 @@
 // timer 3 sec e numeri scompaiono
 // utente inserisce uno ad uno i numeri che si ricorda
 // software controlla quali numeri sono stati inseriti e controlla quuelli indovinati
+
+let rNumbers = [];
+let uNumbers = [];
+
+let div = document.querySelector("main");
+ 
+for (let i = 0; i < 5; i++){
+    let generated  = Math.floor(Math.random() * 100) + 1;
+    rNumbers.push(generated);
+    div.innerHTML += `<div> ${generated} </div>`;
+}
+
+setTimeout (function(){
+    div.innerHTML = ``;
+}, 5 * 1000);
+
+setTimeout (function() {
+    for (let i = 0; i < 5; i++){
+        let chosen = parseInt(prompt("Quali numeri ti ricordi?"));
+        uNumbers.push(chosen);
+    }
+}, 2 * 1000);
+
