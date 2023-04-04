0a6db3b16c5f59afb682936550c7f317a15f02fe
gluke0 <milanesigianluca@gmail.com>
2023-04-04 16:22:19 +0200

    fix prompt blocco setTimeout

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,23 +5,24 @@
 
 let rNumbers = [];
 let uNumbers = [];
+let result = [];
 
-let div = document.querySelector("main");
+let div = document.querySelector(".container");
  
 for (let i = 0; i < 5; i++){
-    let generated  = Math.floor(Math.random() * 100) + 1;
+    let generated  = Math.floor(Math.random() * 99) + 1;
     rNumbers.push(generated);
     div.innerHTML += `<div> ${generated} </div>`;
 }
 
 setTimeout (function(){
     div.innerHTML = ``;
-}, 5 * 1000);
+}, 4000);
 
 setTimeout (function() {
     for (let i = 0; i < 5; i++){
         let chosen = parseInt(prompt("Quali numeri ti ricordi?"));
         uNumbers.push(chosen);
-    }
-}, 2 * 1000);
+    };
+}, 5000);
 
