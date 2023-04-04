8cb3c3c63222baefcc54975675cfa93e9b92c219
gluke0 <milanesigianluca@gmail.com>
2023-04-04 14:08:15 +0200

    setup

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// visualizzare 5 numeri casuali in pagina
+// timer 3 sec e numeri scompaiono
+// utente inserisce uno ad uno i numeri che si ricorda
+// software controlla quali numeri sono stati inseriti e controlla quuelli indovinati
